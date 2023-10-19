84fc8cd109cbb036a6a0c45f612518f6297fba44
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-10-19 22:51:40 +0200

    delete file

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
-import { uploadFile } from "../../../../utils/bucket"; 
+import { uploadFile, deleteFile } from "../../../../utils/bucket";
 import { v4 as uuidv4 } from "uuid";
+import { PrismaClient } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
   try {
+    const prisma = new PrismaClient();
+
     const contentType = req.headers.get("Content-Type");
     const bucketName = process.env.BUCKET_NAME;
     const fileName = `${uuidv4()}.jpg`;
@@ -22,18 +25,72 @@ export async function POST(req: NextRequest) {
     const fileContent = Buffer.from(bytes);
 
     if (bucketName) {
-
       await uploadFile(
         bucketName,
         fileName,
         fileContent,
         contentType ? contentType : "image/png"
       );
+      const image = await prisma.image.create({
+        data: {
+          name: fileName,
+        },
+      });
+      if (!image) {
+        await deleteFile(bucketName, fileName);
+      }
     }
-    return NextResponse.json({ success: true, message: "The image has been sended successfully", image: `${process.env.PUBLIC_DOMAINE_BUCKET_URL}${fileName}` });
-
+    prisma.$disconnect();
+    return NextResponse.json({
+      success: true,
+      message: "The image has been sended successfully",
+      image: `${process.env.PUBLIC_DOMAINE_BUCKET_URL}${fileName}`,
+    });
   } catch (error: any) {
     console.log(error);
     return NextResponse.json({ success: false, message: "serverside error" });
   }
+}
+
+export async function DELETE(req: NextRequest) {
+  const url = new URL(req.url);
+  const searchParams = new URLSearchParams(url.search);
+  const filename = searchParams.get("filename");
+
+  if (!filename) {
+    return NextResponse.json({
+      success: false,
+      message: "Any file has been specified !",
+    });
+  }
+  const prisma = new PrismaClient();
+  const image = await prisma.image.findUnique({
+    where: {
+      name: filename,
+    },
+  });
+  if (image) {
+    const bucketName = process.env.BUCKET_NAME;
+    if (!bucketName) {
+      return NextResponse.json({
+        success: false,
+        message: "Serverside error",
+      });
+    }
+    await deleteFile(bucketName, filename);
+    await prisma.image.delete({
+      where: {
+        name: filename
+      }
+    })
+  }else{
+    return NextResponse.json({
+      success: false,
+      message: "This image does'nt exists",
+    });
+  }
+  return NextResponse.json({
+    success: true,
+    message: "the file has been deleted successfully !",
+  });
 }
--- utils/bucket.ts
+++ utils/bucket.ts
@@ -2,7 +2,41 @@ import {
   S3Client,
   CreateBucketCommand,
   PutObjectCommand,
+  DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
+
+export const deleteFile = async (bucketName: string, filename: string) => {
+  const params = {
+    Bucket: bucketName,
+    Key: filename,
+  };
+
+  const accessKeyId = process.env.ACCESS_KEY_ID;
+  const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
+  if (!accessKeyId || !secretAccessKey) {
+    return;
+  }
+
+  const s3 = new S3Client({
+    region: "auto",
+    endpoint: `https://${process.env.ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  });
+
+  try {
+    const command = new DeleteObjectCommand(params);
+    const response = await s3.send(command);
+    if (!response) {
+      return;
+    }
+  } catch (err: any) {
+    console.error(err);
+  }
+};
 
 export const createBucket = async (bucketName: string) => {
   const accessKeyId = process.env.ACCESS_KEY_ID;
@@ -75,4 +109,3 @@ export const uploadFile = async (
     );
   }
 };
-
