af5f0e3d66ba52d034e4cc50c0acfda9344d1bfb
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-11-07 17:43:33 +0100

    login supp

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        client_jwt_key: "805f2abc6001ba79005e868f3da07eff"
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
