27db4c0681c76a03d39a8c83436db8ea0d9c893f
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-11-23 20:47:59 +0100

    Update sesClient.ts

--- email/sesClient.ts
+++ email/sesClient.ts
@@ -2,7 +2,19 @@ import { SESClientConfig, SESClient } from "@aws-sdk/client-ses";
 
 let sesClient: SESClient;
 
-  if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
+  if (!process.env.AWS_API_KEY || !process.env.AWS_SECRET_API_KEY) {
+    console.error("SES api keys not provided");
+  }
+  const SES_CONFIG: SESClientConfig = {
+    credentials: {
+      accessKeyId: process.env.AWS_API_KEY as string,
+      secretAccessKey: process.env.AWS_SECRET_API_KEY as string,
+    },
+  };
+  sesClient = new SESClient(SES_CONFIG);
+} else {
+  if (!global.sesClient) {
     if (!process.env.AWS_API_KEY || !process.env.AWS_SECRET_API_KEY) {
       console.error("SES api keys not provided");
     }
@@ -20,25 +32,16 @@ let sesClient: SESClient;
       }
       const SES_CONFIG: SESClientConfig = {
         credentials: {
-          accessKeyId: process.env.AWS_API_KEY as string,
-          secretAccessKey: process.env.AWS_SECRET_API_KEY as string,
-        },
-    };
-    sesClient = new SESClient(SES_CONFIG);
-  } else {
-    if (!global.sesClient) {
-      if (!process.env.AWS_API_KEY || !process.env.AWS_SECRET_API_KEY) {
-        console.error("SES api keys not provided");
-      }
-      const SES_CONFIG: SESClientConfig = {
-        credentials: {
-          accessKeyId: process.env.AWS_API_KEY?process.env.AWS_API_KEY:"",
-          secretAccessKey: process.env.AWS_SECRET_API_KEY?process.env.AWS_SECRET_API_KEY:"",
+          accessKeyId: process.env.AWS_API_KEY ? process.env.AWS_API_KEY : "",
+          secretAccessKey: process.env.AWS_SECRET_API_KEY
+            ? process.env.AWS_SECRET_API_KEY
+            : "",
         },
       };
       global.sesClient = new SESClient(SES_CONFIG);
     }
     sesClient = global.sesClient;
   }
+}
 
 export default sesClient;
