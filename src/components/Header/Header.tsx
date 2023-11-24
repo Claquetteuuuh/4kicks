ee5ed49e3aceebbf20cebd849984a80173a63d1e
charles <charles.denner@etu.univ-lyon1.fr>
2023-11-24 09:20:41 +0100

    avancement header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,13 @@ import TextInput from '../TextInput/TextInput';
 import axios from 'axios';
 import styles from "./header.module.css"
 import { useRouter } from 'next/navigation';
+import { ButtonSeConnecter } from '../Button/ButtonSeConnecter';
+import { ButtonMarketplace } from '../Button/ButtonMarketplace';
+import { ButtonPanier } from '../Button/ButtonPanier';
+import { ButtonFavoris } from '../Button/ButtonFavoris';
 
 
-export default function Layout() {
+export default function Header() {
 
     const router = useRouter();
     
@@ -18,11 +22,16 @@ export default function Layout() {
     }  
 
     return <header className={styles.container}>
+        <img src="/imgs/logo.png" alt="logo" className={styles.logo} />
         <div className={styles.search}>
             <form onSubmit={handleForm}>
-                <TextInput placeholder='Rechercher...' state={motCles} setState={setmotCles} className={styles.searchBar} />
+                <TextInput placeholder='Rechercher...' state={motCles} setState={setmotCles} />
                 <button type='submit'>ok</button>
             </form>
         </div>
+        <ButtonSeConnecter />
+        <ButtonMarketplace />
+        <ButtonPanier />
+        <ButtonFavoris />
     </header>
 }
