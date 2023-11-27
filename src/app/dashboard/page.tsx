132edc8abc1d6542c98ac2863bf62c0512af820e
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-11-27 09:15:45 +0100

    init dashboard branche

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import DashboardLayout from '@/components/DashboardLayout/DashboardLayout';
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <DashboardLayout>
+            quoicoumonde
+        </DashboardLayout>
+    );
+};
+
+export default Dashboard;
