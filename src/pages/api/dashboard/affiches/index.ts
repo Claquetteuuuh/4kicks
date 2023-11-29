64d8593335360b61e615b68d8dd0d31a0ec5e24b
versus10-prog <150893635+versus10-prog@users.noreply.github.com>
2023-11-29 15:31:57 +0100

    suppression affiche dashboard

--- src/pages/api/dashboard/affiches/index.ts
+++ src/pages/api/dashboard/affiches/index.ts
@@ -9,4 +9,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
         res.status(200).json(affiches)
     }
+    else if (req.method === "DELETE") {
+        const { affiche_uid } = req.headers;
+        if (!affiche_uid) {
+            res.status(400).json({ error: "affiche uid not specified ! A-001" })
+            return;
+        }
+        const deleted = await prisma.affiche.delete({
+            where: {
+                affiche_uid: affiche_uid as string
+            }
+        })
+        if (deleted) {
+            res.status(200).json({ message: "the affiche has been deleted" });
+        } else {
+            res.status(400).json({ error: "Error during deleting. A-002" })
+        }
+    }
 }
