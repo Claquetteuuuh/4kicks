4fd283e3537e24d1b344b746a7fdd12c779ab8de
Claquetteuuuh <75044328+Claquetteuuuh@users.noreply.github.com>
2023-12-04 19:08:56 +0100

    frontend page produit

--- src/components/plainButton/plainButton.tsx
+++ src/components/plainButton/plainButton.tsx
@@ -7,11 +7,12 @@ type connButtonProps = {
   text: string,
   color?: string,
   type?:  undefined | "submit" | "reset" | "button",
-  onClick?: any
+  onClick?: any,
+  className?: string
 };
-const PlainButton = ({ img, imgAlt, text, color="primary", type="button", onClick }: connButtonProps) => {
+const PlainButton = ({ img, imgAlt, text, color="primary", type="button", onClick, className }: connButtonProps) => {
   return (
-    <button onClick={onClick} type={type} className={`${styles.connectionButton} ${(color == "primary")? styles.primaryColor : styles.otherColor}`}>
+    <button onClick={onClick} type={type} className={`${styles.connectionButton} ${(color == "primary")? styles.primaryColor : styles.otherColor} ${className}`}>
       {img ? <img src={img} alt={imgAlt} /> : false}
       <p>{text}</p>
       {img ? <p className={styles.ghost}></p> : false}
