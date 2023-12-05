5eca3cdad3261febf126417c8341d9cff7493a06
DESKTOP-UJ212KS\charl <charles.denner@etu.univ-lyon1.fr>
2023-12-05 23:26:22 +0100

    petite modif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import axios from "axios";
 import { useSearchParams } from "next/dist/client/components/navigation";
 import { ArticlesType } from "../../types/home/Article";
 import { AfficheType } from "../../types/home/Affiche";
+import { ProduitType } from "../../types/home/Produit"
 import test from "node:test";
 import Slider from "../components/Slider/Slider"
 import { Component } from "lucide-react";
@@ -17,6 +18,7 @@ import Footer from "@/components/footer/Footer";
 export default function Home({ params }: { params: { user: userType } }) {
   const [dataSearch, setdataSearch] = useState<ArticlesType[]>([]);
   const [affiches, setAffiches] = useState<AfficheType[]>([]);
+  const [categorie, setCategorie] = useState<ProduitType[]>([]);
 
   const parametre = useSearchParams();
 
@@ -25,7 +27,7 @@ export default function Home({ params }: { params: { user: userType } }) {
       axios
         .get(`/api/affiches`)
         .then((response) => {
-          console.log(response);
+         // console.log(response);
           setAffiches(response.data);
         })
         .catch((error) => {
@@ -33,6 +35,20 @@ export default function Home({ params }: { params: { user: userType } }) {
         });
     }
   }, [affiches]);
+
+  useEffect(() => {
+    if (categorie.length === 0) {
+      axios
+        .get(`/api/produit`)
+        .then((response) => {
+          console.log(response);
+          setCategorie(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [categorie]);
 
 
   useEffect(() => {
@@ -107,6 +123,22 @@ export default function Home({ params }: { params: { user: userType } }) {
         </div>
       </div>
       <Footer />
+      <div className={styles.container_header}>
+          <div className={styles.article_container}>
+            <h1>Nouveautés</h1>
+            {categorie ? (
+              categorie.map((cate: ProduitType) => {
+                return (
+                  <div key={cate.productUID} className={styles.container}>
+                    
+                  </div>
+                );
+              })
+            ) : (
+              <p>Pas de donnee</p>
+            )}
+          </div>
+        </div>
     </CheckAccountLayout>
 
   );
