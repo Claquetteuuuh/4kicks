2fe4df1a2eea2df6c89abd5baf0a0593b14ac3cf
charles <charles.denner@etu.univ-lyon1.fr>
2023-12-06 16:48:38 +0100

    avancement favoris

--- src/app/favoris/page.tsx
+++ src/app/favoris/page.tsx
@@ -8,22 +8,48 @@ import ProductRecherche from '@/components/ProductRecherche/ProductRecherche';
 import Loading from '@/components/Loading/Loading';
 import CheckAccountLayout from '@/components/checkAccountLayout/CheckAccountLayout';
 import ProductCategories from '@/components/ProductCategories/ProductCategories';
+import { FullProductType } from '../../../types/product/Product';
 
 
 export default function Recherche({ params }: { params: { user: userType } }) {
     const [favoris, setFavoris] = useState<ProduitType[]>([]);
+    const [recom, setRecom] = useState<ProduitType[]>([]);
 
     useEffect(() => {
         if(favoris.length === 0) {
             axios
                 .get(`/api/favoris?userID=${params.user.email}`)
+                .then((response) => {
+                    console.log(response);
+                    setFavoris(response.data)
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        }
+    }, [favoris])
+
+    useEffect(() => {
+        if(recom.length > 0) {
+            axios
+                .get(`/api/recommandations-favoris?userID=${params.user.email}`)
+                .then((response) => {
+                    console.log(response)
+                    setRecom(response.data)
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         }
     })
 
 
+
+
   return (
     <CheckAccountLayout user={params.user}>
-        <ProductCategories allProducts={} />
+        <ProductCategories allProducts={favoris} name="Favoris" />
+        <ProductCategories allProducts={recom} name="Découverte" />
     </CheckAccountLayout>
   );
 }
