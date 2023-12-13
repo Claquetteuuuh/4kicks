f6cb0d250327b47605ea6ced06e5a8c0496b11e8
versus10-prog <150893635+versus10-prog@users.noreply.github.com>
2023-12-13 12:10:27 +0100

    recomendation tri par categorie

--- src/pages/api/recommandation/index.ts
+++ src/pages/api/recommandation/index.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { ProduitType } from "../../../../types/home/Produit";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const host = req.headers.host;
@@ -13,15 +12,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === "GET") {
 
         const userUID: string = req.query.userID as string;
-        const finalList: filtre[] = await recuperation_filtre(userUID);
-
+        //liste produits favoris
+        const finalList: filtreCat[] = await recuperation_filtre(userUID);
+        //liste des uid des categories
+        const quatreClesMax: string[] = await filtreCategorie(finalList);
+        //liste des produits qui possede une des 4 catégories
+        const produitTrieCat: filtreMarque[] = await trieproduitCategorie(quatreClesMax);
     }
 }
 
-
+//recupere les categorie de tout les produits en favoris
 async function recuperation_filtre(userUID: string) {
-    const prisma: PrismaClient = new PrismaClient();
-    const favorisAcount: filtre[] = [];
+    const favorisAcount: filtreCat[] = [];
     const favoris = await prisma.favorite.findMany({
         select: {
             product_uid: true
@@ -35,8 +37,7 @@ async function recuperation_filtre(userUID: string) {
     await Promise.all(favoris.map(async (favori) => {
         const produit = await prisma.product.findMany({
             select: {
-                name: true,
-                marque: true,
+                product_uid: true,
                 product_categorie: {
                     select: {
                         categorie: {
@@ -63,10 +64,9 @@ async function recuperation_filtre(userUID: string) {
                 }
             })
 
-            const product: filtre = {
-                name: thisProduit.name,
-                categorie: categorieListe,
-                marque: thisProduit.marque
+            const product: filtreCat = {
+                product_uid: thisProduit.product_uid,
+                categorie: categorieListe
             };
             favorisAcount.push(product);
         });
@@ -75,16 +75,97 @@ async function recuperation_filtre(userUID: string) {
     return favorisAcount;
 }
 
-async function filtreCategorie()
+//renvoie les 4 categories preferé 
+async function filtreCategorie(listeProduits: filtreCat[]) {
+    let categorieMap = await categoriesMap();
+
+    // Parcour des produits
+    listeProduits.forEach(thisProduit => {
+
+        let listeCat = thisProduit.categorie;
+
+        //Parcour des catégories dans les produits
+        listeCat.forEach(thisCat => {
+            const nb = categorieMap.get(thisCat.categorie_id) + 1;
+            categorieMap.set(thisCat.categorie_id, nb);
+
+        })
+    })
+
+    const sortedEntries = Array.from(categorieMap.entries()).sort((a, b) => b[1] - a[1]);
+
+    // Extraire les quatre premières clés
+    const quatreClesMax = sortedEntries.slice(0, 4).map(entry => entry[0]);
+
+    return quatreClesMax;
+}
+
+// renvoie un map des categorie uid avec comme value 0
+async function categoriesMap() {
+    let tableauxCategorie = new Map();
+
+    const requete = await prisma?.categorie.findMany({
+        select: {
+            name: true,
+            categorie_uid: true
+        }
+    });
+
+    requete?.forEach(thisCat => {
+        tableauxCategorie.set(thisCat.categorie_uid, 0)
+    })
+
+    return tableauxCategorie;
+}
+
+// retourne une liste de produits correspondant au categories
+async function trieproduitCategorie(listeCategories: string[]) {
+    const listeProduct = await prisma?.product.findMany({
+        select: {
+            product_uid: true,
+            marque: true,
+            name: true,
+
+        },
+        where: {
+            product_categorie: {
+                some: {
+                    categorie_uid: {
+                        in: listeCategories
+                    }
+                }
+            }
+        }
+    });
+
+    let produits: filtreMarque[] = [];
+    listeProduct.forEach(thoisProduct => {
+        const produit: filtreMarque = {
+            product_uid: thoisProduct.product_uid,
+            marque: thoisProduct.marque,
+            name: thoisProduct.name
+        };
+
+        produits.push(produit);
+    });
+
+    return produits;
+}
 
 
-type filtre = {
-    categorie: categorie[],
-    marque: string
-    name: string,
+
+
+type filtreCat = {
+    product_uid: string,
+    categorie: categorie[]
 }
 type categorie = {
     categorie_id: string,
+    name: string
+}
+type filtreMarque = {
+    product_uid: string,
+    marque: string,
     name: string
 }
 
