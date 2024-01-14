8b96240e391bcb4e87aafbbb69fe0015aefa3217
versus10-prog <150893635+versus10-prog@users.noreply.github.com>
2024-01-14 21:59:14 +0100

    suppression image fini

--- src/pages/api/dashboard/products/index.ts
+++ src/pages/api/dashboard/products/index.ts
@@ -1,10 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
-import { METHODS } from "http";
-import { NextResponse } from "next/server";
-import { uploadFile } from "@/lib/bucket";
-import { v4 as uuidv4 } from 'uuid';
-import { ProductImages } from "@prisma/client";
+import suppressionImages from "../commun/suppressionImages";
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
@@ -16,25 +13,48 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     } else if (req.method === "DELETE") {
         const product_uid = req.headers["product_uid"] as string;
 
-        console.log("testttttt " + product_uid)
         if (!product_uid) {
             res.status(400).json({ error: "product uid not specified ! P-001" })
             return;
         }
+        // Recuperation des images
+        const images = await prisma.image.findMany({
+            select: {
+                image_uid: true,
+                name: true
+            },
+            where: {
+                product_images: {
+                    some: {
+                        product_uid: product_uid
+                    }
+                }
+            }
+
+        })
+
+        //suppression du product image
         const deletedProductImage = await prisma.productImages.deleteMany({
             where: {
                 product_uid: product_uid
             }
         })
         if (deletedProductImage) {
-            const deleted = await prisma.product.delete({
-                where: {
-                    product_uid: product_uid
+
+            //suppression des images dans la bd et dans le buckets
+            if (suppressionImages(images)) {
+                const deleted = await prisma.product.delete({
+                    where: {
+                        product_uid: product_uid
+                    }
+                })
+                if (deleted) {
+                    res.status(200).json({ message: "the product has been deleted" });
+                } else {
+                    res.status(400).json({ error: "Error during deleting. P-002" })
                 }
-            })
-            if (deleted) {
-                res.status(200).json({ message: "the product has been deleted" });
-            } else {
+            }
+            else{
                 res.status(400).json({ error: "Error during deleting. P-002" })
             }
         }
